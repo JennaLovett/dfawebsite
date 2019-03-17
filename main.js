79b8ee6c6e38357e7135895805fa8cd83896df79
Jenna Lovett <jennablair12@gmail.com>
2019-03-16 23:06:28 -0400

    working on transition table

--- main.js
+++ main.js
@@ -1,59 +1,73 @@
 window.onload=function() {
 
-//creating variables
-var alphabet = [];
-var states = [];
-var startState;
-var acceptStates = [];
-var transitions = [];
+    //creating variables
+    var alphabet = [];
+    var states = [];
+    var startState;
+    var acceptStates = [];
+    var transitions = [];
+    var transitionTable;
 
-//adding event listeners and getting input
-document.getElementById("btn1").addEventListener("click", () => {
-    var input = document.getElementById("alphabet").value;
-    for(var i = 0; i < input.length; i++) {
-        if(input.substring(i, i+1) == "," || input.substring(i, i+1) == " ") {
-            continue;
-        } else {
-            alphabet.push(input.substring(i, i+1));
+    //adding event listeners and getting input
+    document.getElementById("btn1").addEventListener("click", () => {
+        var input = document.getElementById("alphabet").value;
+        for(var i = 0; i < input.length; i++) {
+            if(input.substring(i, i+1) == "," || input.substring(i, i+1) == " ") {
+                continue;
+            } else {
+                alphabet.push(input.substring(i, i+1));
+            }
+        }
+    });
+
+    document.getElementById("btn2").addEventListener("click", () => {
+        var input = document.getElementById("states").value;
+        for(var i = 0; i < input.length; i++) {
+            if(input.substring(i, i+1) == "," || input.substring(i, i+1) == " ") {
+                continue;
+            } else {
+                states.push(input.substring(i, i+1));
+            }
+        }
+    });
+
+    document.getElementById("btn3").addEventListener("click", () => {
+        var input = document.getElementById("start").value;
+        startState = input;
+    });
+
+    document.getElementById("btn4").addEventListener("click", () => {
+        var input = document.getElementById("accept").value;
+        for(var i = 0; i < input.length; i++) {
+            if(input.substring(i, i+1) == "," || input.substring(i, i+1) == " ") {
+                continue;
+            } else {
+                acceptStates.push(input.substring(i, i+1));
+            }
+        }
+    });
+
+    document.getElementById("btn5").addEventListener("click", () => {
+        var input = document.getElementById("transitions").value;
+        for(var i = 0; i < input.length; i++) {
+            if(input.substring(i, i+1) == "," || input.substring(i, i+1) == " ") {
+                continue;
+            } else {
+                transitions.push(input.substring(i, i+1));
+            }
+        }
+
+        createTransitionTable();
+    });
+
+    function createTransitionTable() {
+        transitionTable = new Array(states.length);
+        for(var i = 0; i < transitionTable.length; i++) {
+            transitionTable[i] = new Array(alphabet.length);
         }
     }
-});
 
-document.getElementById("btn2").addEventListener("click", () => {
-    var input = document.getElementById("states").value;
-    for(var i = 0; i < input.length; i++) {
-        if(input.substring(i, i+1) == "," || input.substring(i, i+1) == " ") {
-            continue;
-        } else {
-            states.push(input.substring(i, i+1));
-        }
+    function fillTransitionTable() {
+        
     }
-});
-
-document.getElementById("btn3").addEventListener("click", () => {
-    var input = document.getElementById("start").value;
-    startState = input;
-});
-
-document.getElementById("btn4").addEventListener("click", () => {
-    var input = document.getElementById("accept").value;
-    for(var i = 0; i < input.length; i++) {
-        if(input.substring(i, i+1) == "," || input.substring(i, i+1) == " ") {
-            continue;
-        } else {
-            acceptStates.push(input.substring(i, i+1));
-        }
-    }
-});
-
-document.getElementById("btn5").addEventListener("click", () => {
-    var input = document.getElementById("transitions").value;
-    for(var i = 0; i < input.length; i++) {
-        if(input.substring(i, i+1) == "," || input.substring(i, i+1) == " ") {
-            continue;
-        } else {
-            transitions.push(input.substring(i, i+1));
-        }
-    }
-});
 }
